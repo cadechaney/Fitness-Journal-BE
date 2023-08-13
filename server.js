e14bc35f6f309ff11a35c125f8761016312d07c0
Cade Chaney <cadechaney@live.com>
2023-08-13 15:49:50 -0600

    Add rendering of the workouts.json file with a readfile

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const express = require('express')
 const app = express()
+const fs = require('fs')
 
 app.get('/stuff', (req, res) => {
-  res.json({ "stuff": ['this', 'that', 'then'] })
+  fs.readFile('./workouts.json', 'utf-8', (err, jsonString) => {
+    if(err) {
+      console.log(err)
+      res.status(500).json({ error: 'An error occured while reading the file'})
+    } else {
+      try {
+        const data = JSON.parse(jsonString);
+        console.log(data)
+        res.json({ workouts: data})
+      } catch (err) {
+        console.log('Error parsing JSON:', err)
+        res.status(500).json({ error: 'An error occured while parsing JSON'})
+      }
+    }
+  })
 })
 
 app.listen(5000, () => {console.log("Server started on port 5000")})
