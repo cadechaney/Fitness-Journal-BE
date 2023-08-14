892a89aa74db41ad4624361ec6f93e68233c325b
Cade Chaney <cadechaney@live.com>
2023-08-14 05:19:21 -0600

    Add successful post

--- server.js
+++ server.js
@@ -1,27 +1,55 @@
-const express = require('express')
-const app = express()
-const fs = require('fs')
+const express = require('express');
+const app = express();
+const fs = require('fs');
+
+app.use(express.json()); // Add this line to parse JSON in the request body
 
 app.get('/stuff', (req, res) => {
   fs.readFile('./workouts.json', 'utf-8', (err, jsonString) => {
-    if(err) {
-      console.log(err)
-      res.status(500).json({ error: 'An error occured while reading the file'})
+    if (err) {
+      console.log(err);
+      res.status(500).json({ error: 'An error occurred while reading the file' });
     } else {
       try {
         const data = JSON.parse(jsonString);
-        console.log(data)
-        res.json({ workouts: data})
+        res.json({ workouts: data });
       } catch (err) {
-        console.log('Error parsing JSON:', err)
-        res.status(500).json({ error: 'An error occured while parsing JSON'})
+        console.log('Error parsing JSON:', err);
+        res.status(500).json({ error: 'An error occurred while parsing JSON' });
       }
     }
-  })
-})
+  });
+});
 
 app.post('/stuff', (req, res) => {
-  res.send("Post was successful")
-})
+  const newData = req.body.workouts.stuff;
+  console.log('Received new data', newData);
 
-app.listen(5000, () => {console.log("Server started on port 5000")})
+  fs.readFile('./workouts.json', 'utf-8', (err, jsonString) => {
+    if (err) {
+      console.log('Error reading file:', err);
+      res.status(500).json({ error: 'An error occurred while reading the file' });
+    } else {
+      try {
+        const existingData = JSON.parse(jsonString);
+        existingData.stuff = newData;
+
+        fs.writeFile('./workouts.json', JSON.stringify(existingData, null, 2), 'utf-8', writeErr => {
+          if (writeErr) {
+            console.error('Error writing to workouts.json:', writeErr);
+            res.status(500).json({ error: 'An error occurred while writing to file' });
+          } else {
+            res.json({ message: 'Post was successful', workouts: existingData });
+          }
+        });
+      } catch (parseErr) {
+        console.log('Error parsing JSON:', parseErr);
+        res.status(500).json({ error: 'An error occurred while parsing JSON' });
+      }
+    }
+  });
+});
+
+app.listen(5000, () => {
+  console.log('Server started on port 5000');
+});
